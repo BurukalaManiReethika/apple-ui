f1abffd617037354bfefa5e010b8142d58969166
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-30 10:59:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import Stats from "./components/Stats";
+import BentoGrid from "./components/BentoGrid";
+import CTA from "./components/CTA";
 import Footer from "./components/Footer";
 
-export default function App(){
+export default function App() {
+  return (
+    <div className="relative overflow-hidden">
 
-return(
+      {/* Background Glow */}
+      <div className="absolute inset-0 -z-10 overflow-hidden">
+        <div className="absolute top-0 left-1/2 h-[500px] w-[500px] -translate-x-1/2 rounded-full bg-blue-500/20 blur-[150px]" />
+        <div className="absolute bottom-0 right-0 h-[400px] w-[400px] rounded-full bg-purple-500/20 blur-[150px]" />
+      </div>
 
-<div>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Stats />
+      <BentoGrid />
+      <CTA />
+      <Footer />
 
-<Navbar/>
-
-<Hero/>
-
-<Features/>
-
-<Footer/>
-
-</div>
-
-)
-
+    </div>
+  );
 }
